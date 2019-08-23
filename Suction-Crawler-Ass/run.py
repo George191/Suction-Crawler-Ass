53772b28a932f51291a6909d1150ca1ef0864b7d
George191 <41810038+George191@users.noreply.github.com>
2019-08-23 18:17:14 +0800

    Update run.py

--- Suction-Crawler-Ass/run.py
+++ Suction-Crawler-Ass/run.py
@@ -1,13 +1,28 @@
 from crawler.LiePin import CompanyInfo
+import pandas as pd
+from tools.proxy import Proxy
+import os
 
 
 class QiChaCha(CompanyInfo):
 
     def __init__(self):
         super(QiChaCha, self).__init__()
+        self.analysis_city()
+        self.name = None
+        self.config = Proxy()
 
-        self.analysis_html()
+    def company(self, path=None):
+        if not path:
+            path = self.config.proxies(key='LiePinData')
+        self.name = pd.read_csv(os.path.join(os.getcwd(), path))['company_name'].tolist()
+
+    # def analysis_QiChaCha(self):
 
 
-demo = QiChaCha()
+if __name__ == '__main__':
+    demo = QiChaCha()
+
+
+
 
